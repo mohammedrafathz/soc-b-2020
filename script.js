ec2093f0880294fd24890074b90811746f04c795
risky-boy <smart.rafath@gmail.com>
2020-12-14 12:37:11 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+// if I want to repeat something
+// print 0 to 10
+
+// var i = 0;
+// while (i <= 10) {
+//     debugger;
+
+//     if(i >= 10){
+//         break;
+//     }
+
+//     console.log(i);
+//     i = i + 1;
+// }
+
+// while (true){
+
+//     console.log("looping via while")
+// }
+
+// do{
+//     console.log('Running once');
+// }while(false)
+
+// var i = 0;
+
+// do {
+
+//     if(i >=10){
+//         break
+//     }
+
+//     console.log(i)
+// i = i + 1;
+// } while (i <= 10);
+
+
+// for (var i = 0; i <= 10 ; i++) {
+//     console.log(i);
+// }
+
+// try {
+//     // console.log(myVar1)
+//     throw("error");
+// } catch (error) {
+//     console.log("trigger error via throw keyword")
+// }
+
+'use strict'
+
+var x = 1;
+
+console.log(x)
